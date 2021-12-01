e0c76a3bbb75d79df3dedbec42f8bf732052ee6b
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-12-01 13:46:14 +0000

    Simplified

--- src/Credfeto.UrlShortener/ShortenerFactory.cs
+++ src/Credfeto.UrlShortener/ShortenerFactory.cs
@@ -3,41 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using Credfeto.UrlShortener.Shorteners;
 
-namespace Credfeto.UrlShortener
+namespace Credfeto.UrlShortener;
+
+/// <summary>
+///     Factory for creating Url Shorteners.
+/// </summary>
+public sealed class ShortenerFactory : IShortenerFactory
 {
+    private readonly IReadOnlyList<IUrlShortener> _shorteners;
+
     /// <summary>
-    ///     Factory for creating Url Shorteners.
+    ///     Constructor.
     /// </summary>
-    public sealed class ShortenerFactory : IShortenerFactory
+    /// <param name="shorteners">Registered shorteners.</param>
+    public ShortenerFactory(IEnumerable<IUrlShortener> shorteners)
     {
-        private readonly IReadOnlyList<IUrlShortener> _shorteners;
+        this._shorteners = shorteners.ToArray();
+    }
 
-        /// <summary>
-        ///     Constructor.
-        /// </summary>
-        /// <param name="shorteners">Registered shorteners.</param>
-        public ShortenerFactory(IEnumerable<IUrlShortener> shorteners)
-        {
-            this._shorteners = shorteners.ToArray();
-        }
+    /// <summary>
+    ///     Creates an instance of the specified publisher.
+    /// </summary>
+    /// <param name="type">
+    ///     The type of the publisher to create.
+    /// </param>
+    /// <returns>
+    ///     The requested publisher if a matching one can be found; otherwise, <see langword="null" />.
+    /// </returns>
+    public IUrlShortener Create(string type)
+    {
+        return this._shorteners.FirstOrDefault(s => StringComparer.OrdinalIgnoreCase.Equals(x: s.Name, y: type)) ?? new Generic(DoNotShorten);
+    }
 
-        /// <summary>
-        ///     Creates an instance of the specified publisher.
-        /// </summary>
-        /// <param name="type">
-        ///     The type of the publisher to create.
-        /// </param>
-        /// <returns>
-        ///     The requested publisher if a matching one can be found; otherwise, <see langword="null" />.
-        /// </returns>
-        public IUrlShortener Create(string type)
-        {
-            return this._shorteners.FirstOrDefault(s => s.Name == type) ?? new Generic(DoNotShorten);
-        }
-
-        private static Uri DoNotShorten(Uri fullUrl)
-        {
-            return fullUrl;
-        }
+    private static Uri DoNotShorten(Uri fullUrl)
+    {
+        return fullUrl;
     }
 }
